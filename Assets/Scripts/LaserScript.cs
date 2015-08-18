8a84f8d69ef12e8307b74fbdfaa43419abe7b946
nliampisan <nliampisan@gmail.com>
2015-08-18 19:42:24 +0700

    first commit

---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class LaserScript : MonoBehaviour {
+
+	private int count; 
+	public Text countText; 
+
+
+	LineRenderer line; 
+	// Use this for initialization
+	void Start () {
+		count = 0; 
+		countText.text = "Score: " + count.ToString ();  
+		line = gameObject.GetComponent<LineRenderer>(); 
+		line.enabled = false; 
+
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false; 
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			StopCoroutine ("FireLaser"); 
+			StartCoroutine ("FireLaser"); 
+		}
+	
+	}
+
+	IEnumerator FireLaser() 
+	{
+		line.enabled = true; 
+		while (Input.GetButton("Fire1")) {
+
+
+			Ray ray = new Ray(transform.position, transform.forward); 
+			RaycastHit hit; 
+
+			line.SetPosition(0, ray.origin); 
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				line.SetPosition (1, hit.point); 
+				if(hit.rigidbody)
+				{
+					if(hit.rigidbody.gameObject.CompareTag("Target")) {
+						hit.rigidbody.gameObject.SetActive(false); 
+						count = count+10; 
+						countText.text = "Score: " + count.ToString ();  
+					}
+					else if(hit.rigidbody.gameObject.CompareTag("Neg"))
+					{
+						hit.rigidbody.gameObject.SetActive(false); 
+						count = count-5; 
+						countText.text = "Score: " + count.ToString ();
+					}
+					else
+							hit.rigidbody.AddForceAtPosition(transform.forward*20, hit.point); 
+				}
+			}
+			else
+				line.SetPosition (1, ray.GetPoint (100)); 
+
+			yield return null; 
+		}
+
+		line.enabled = false; 
+	}
+
+
+
+}
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+
+public class PickUp : MonoBehaviour {
+
+	public Text countText; 
+	public Text centerText; 
+	public float mytime;
+
+
+
+	void Start() 
+	{
+		mytime = 15.0f; 
+		centerText.text = ""; 
+		countText.text = "Time: " + mytime.ToString ("0.00") + " secs" ; 
+	}
+
+	void Update() 
+	{
+		if (mytime < 0.0f) {
+			centerText.text = "Game Over"; 
+		} else {
+			countText.text = "Time: " + mytime.ToString ("0.00") + " secs"; 
+		}
+
+		mytime -= Time.deltaTime; 
+
+		if (mytime < -0.5f) {
+			Application.LoadLevel(0); 
+		}
+		
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Time"))
+		{
+			other.gameObject.SetActive (false);
+			mytime = mytime+10.0f; 
+		}
+	}
+}
